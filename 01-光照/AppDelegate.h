7f07fc472a26e4d1b6823476e7d329eff41260dc
C C <liuyi@CCdeMacBook-Pro.local>
2018-01-20 21:58:50 +0800

    Initial Commit

---
+++ 01-光照/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01-光照
+//
+//  Created by CC老师 on 2018/1/20.
+//  Copyright © 2018年 CC老师. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01-光照/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01-光照
+//
+//  Created by CC老师 on 2018/1/20.
+//  Copyright © 2018年 CC老师. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
